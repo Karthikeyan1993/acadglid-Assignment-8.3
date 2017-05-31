a7619e941b3abff3e5991c0c2479df65ab23035b
Karthikeyan1993 <karthik.infotec99@gmail.com>
2017-05-31 09:36:37 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+/**
+ * Created by USER on 25/05/2017.
+ */
+$(document).ready(function () {
+    /**
+     * form hide function
+     */
+    $("#btn1").click(function () {
+        $("#form1").hide();
+    });
+    /**
+     * form show function
+     */
+    $("#btn2").click(function () {
+        $("#form1").show();
+    });
+});
